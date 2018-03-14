27ceb4cb8a5e1b4c5e683a465fea8edb386b29c0
Artem Grygoriev <agrygoriev@gmail.com>
2018-03-14 12:51:05 +0200

    ES6 features removed due freeze and bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,196 +1,160 @@
-const windowWidth = window.innerWidth;
-const header = document.getElementsByTagName("header")[0];
-const doc = document.documentElement;
-const navMenu = document.getElementsByClassName("menu-list");
-const menuLinks = navMenu[0].getElementsByTagName("a");
-// const langSwitch = document.getElementsByClassName("lang-switch")[0];
-// const langSwitchBorder = langSwitch.firstElementChild;
-const mobileMenu = document.querySelector(".mobile-menu");
-const colorWhite = "#ffffff";
-const colorBlack = "#202020";
-const colorGreen = "#00d9a2";
-const changeColor = (elementsList, elColor) => {
-  const array = Array.from(elementsList);
-  array.forEach(el => {
+var windowWidth = window.innerWidth;
+var header = document.getElementsByTagName("header")[0];
+var doc = document.documentElement;
+var navMenu = document.getElementsByClassName("menu-list");
+var menuLinks = navMenu[0].getElementsByTagName("a");
+var mobileMenu = document.querySelector(".mobile-menu");
+var colorWhite = "#ffffff";
+var colorBlack = "#202020";
+var colorGreen = "#00d9a2";
+var sideButton = $("header .side-button");
+var closeSideButton = $("header .side-button .close-side-button");
+function changeColor(elementsList, elColor) {
+  var array = Array.from(elementsList);
+  array.forEach(function(el)  {
     el.style.color = elColor;
   });
+}
+var deadline = new Date(2018, 3, 20);
+function windowScrolled() {
+  $("nav .logo svg > path.logo-fill0").addClass("logo-fill-black");
+  header.style.background = colorWhite;
+  header.style.zIndex = "100";
+  header.style["box-shadow"] = "0 0 14px 0 rgba(41, 70, 112, .25)";
+  mobileMenu.style.background = colorWhite;
+  mobileMenu.style.boxShadow = "2px 2px 18px rgba(32, 32, 32, .25)";
+  $(".sandwich span").css("background-color", colorBlack);
+  $("nav .logo > svg path.st0").css("fill", colorBlack);
+  changeColor(navMenu, colorBlack);
+  $(menuLinks).addClass("black-color");
+};
+function windowUnscrolled() {
+  $("nav .logo svg > path.logo-fill0").removeClass("logo-fill-black");
+  header.style.background = "none";
+  header.style["box-shadow"] = "none";
+  mobileMenu.style.background = "none";
+  mobileMenu.style.boxShadow = "none";
+  $(".sandwich span").css("background-color", colorWhite);
+  $("nav .logo > svg path.st0").css("fill", colorWhite);
+  changeColor(navMenu, colorWhite);
+  $(menuLinks).removeClass("black-color");
+}
+function getTimeRemaining(endtime) {
+  var remainingTimeInMs = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((remainingTimeInMs / 1000) % 60);
+  var minutes = Math.floor((remainingTimeInMs / 1000 / 60) % 60);
+  var hours = Math.floor((remainingTimeInMs / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(remainingTimeInMs / (1000 * 60 * 60 * 24));
+  return {
+    total: remainingTimeInMs,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+function initializeClock(id, endtime) {
+  var clock = document.querySelector(id);
+  var daysSpan = clock.querySelector(".days");
+  var hoursSpan = clock.querySelector(".hours");
+  var minutesSpan = clock.querySelector(".minutes");
+  var secondsSpan = clock.querySelector(".seconds");
+  function updateClock() {
+    var time = getTimeRemaining(endtime);
+    var timeInterval = setInterval(updateClock, 1000);
+    daysSpan.innerHTML = `${time.days}`;
+    hoursSpan.innerHTML = `0${time.hours}`.slice(-2);
+    minutesSpan.innerHTML = `0${time.minutes}`.slice(-2);
+    secondsSpan.innerHTML = `0${time.seconds}`.slice(-2);
+
+    if (time.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  };
+  updateClock();
 };
 $(document).ready(function() {
-  $(".close-side-btn").on("click", () => {
-    console.log("Closed side-button clicked!");
-    $("header .side-button").addClass("closed");
-    $("header .side-button .close-side-btn").toggle(200);
+  initializeClock(".counter", deadline);
+  initializeClock("#counter-2", deadline);
+
+  $(".close-side-btn").click(function() {
+    sideButton.addClass("closed");
+    closeSideButton.toggle(200);
   });
-  $("header .side-button img").click(() => {
-    $("header .side-button").toggleClass("closed");
-    $("header .side-button .close-side-btn").toggle(200);
-    $("header .side-button .col").show();
+  $("header .side-button img").click(function() {
+    sideButton.toggleClass("closed");
+    closeSideButton.toggle(200);
   });
-  $(".sandwich").click(() => {
-    $(this).toggleClass("close-btn");
+  $(".sandwich").click(function() {
+    $(".sandwich").toggleClass("sandwich-open");
+    $("nav .logo svg > path.logo-fill0").toggleClass("logo-fill-black");
+    $("nav .logo").css("z-index", "150");
     $(".overlay-menu").toggleClass("overlay-active");
     $("header .sandwich span").toggleClass("dark-background");
   });
-  window.onscroll = () => {
-    const scroll = (this.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+  $(window).scroll(function (ev) {
+    var scroll = $(window).scrollTop();
+    // console.log('Window is scrolled for '+ scroll + ' px');
     if (scroll > 10) {
-      $("nav .logo svg > path.logo-fill0").addClass("logo-fill-black");
-      header.style.background = colorWhite;
-      header.style.zIndex = "100";
-      header.style["box-shadow"] = "0 0 14px 0 rgba(41, 70, 112, .25)";
-      mobileMenu.style.background = colorWhite;
-      mobileMenu.style.boxShadow = "2px 2px 18px rgba(32, 32, 32, .25)";
-      // $("nav .lang-switch").css("color", colorBlack);
-      // $("nav .lang-switch select").css("border-color", colorBlack);
-      $(".sandwich span").css("background-color", colorBlack);
-      $("nav .logo > svg path.st0").css("fill", colorBlack);
-      // langSwitchBorder.style.borderColor = colorBlack;
-      // changeColor(langSwitch, colorBlack);
-      changeColor(navMenu, colorBlack);
-      $(menuLinks).addClass("black-color");
+      windowScrolled();
     } else {
-      $("nav .logo svg > path.logo-fill0").removeClass("logo-fill-black");
-      header.style.background = "none";
-      header.style["box-shadow"] = "none";
-      mobileMenu.style.background = "none";
-      mobileMenu.style.boxShadow = "none";
-      // $("nav .lang-switch").css("color", colorWhite);
-      // $("nav .lang-switch select").css("border-color", colorWhite);
-      $(".sandwich span").css("background-color", colorWhite);
-      $("nav .logo > svg path.st0").css("fill", colorWhite);
-      changeColor(navMenu, colorWhite);
-      // changeColor(langSwitch, colorWhite);
-      // langSwitchBorder.style.borderColor = colorWhite;
-      $(menuLinks).removeClass("black-color");
+      windowUnscrolled();
     }
-  };
-
-  window.onload = () => {
-    const getTimeRemaining = endtime => {
-      const remainingTimeInMs = Date.parse(endtime) - Date.parse(new Date());
-      const seconds = Math.floor((remainingTimeInMs / 1000) % 60);
-      const minutes = Math.floor((remainingTimeInMs / 1000 / 60) % 60);
-      const hours = Math.floor((remainingTimeInMs / (1000 * 60 * 60)) % 24);
-      const days = Math.floor(remainingTimeInMs / (1000 * 60 * 60 * 24));
-      return {
-        total: remainingTimeInMs,
-        days,
-        hours,
-        minutes,
-        seconds
-      };
-    };
-
-    const initializeClock = (id, endtime) => {
-      const clock = document.querySelector(id);
-      const daysSpan = clock.querySelector(".days");
-      const hoursSpan = clock.querySelector(".hours");
-      const minutesSpan = clock.querySelector(".minutes");
-      const secondsSpan = clock.querySelector(".seconds");
-      const updateClock = () => {
-        const time = getTimeRemaining(endtime);
-        const timeInterval = setInterval(updateClock, 1000);
-        daysSpan.innerHTML = `${time.days}`;
-        hoursSpan.innerHTML = `0${time.hours}`.slice(-2);
-        minutesSpan.innerHTML = `0${time.minutes}`.slice(-2);
-        secondsSpan.innerHTML = `0${time.seconds}`.slice(-2);
-
-        if (time.total <= 0) {
-          clearInterval(timeInterval);
-        }
-      };
-      updateClock();
-    };
-
-    const deadline = new Date(2018, 3, 20);
-    initializeClock(".counter", deadline);
-    initializeClock("#counter-2", deadline);
-  };
-
-  $(".overlay-menu ul li a, .desktop-menu ul.menu-list > li > a").click(ev => {
-    // const offset = $(this).offset();
-    const hrefGoTo = $(".desktop-menu ul.menu-list > li > a").attr("href");
-    console.log(hrefGoTo.offset());
-    let position = hrefGoTo.offset.top - 90;
-    ev.preventDefault();
-    if (windowWidth <= 1024) {
-      $(".overlay-menu").toggleClass("overlay-active");
-      $(".sandwich").toggleClass("close-btn");
-    } else if (windowWidth <= 375) {
-      position = $(hrefGoTo).offset().top - 60;
-    }
-    $("html, body").animate({ scrollTop: position }, "slow");
   });
-
-  // const prevArrow =
-  //   '<span class="slide-prev"><svg class="prev-button arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 40"><path d="M4.2 40L.1 34.9l14-14.5L0 5l4.4-5L23 20.5 4.2 40z"/></svg></span>';
-  // const nextArrow =
-  //   '<span class="slide-next"><svg class="next-button arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 40"><path d="M4.2 40L.1 34.9l14-14.5L0 5l4.4-5L23 20.5 4.2 40z"/></svg></span>';
-
-  if (windowWidth <= 1024) {
-    if (windowWidth < 600) {
-      $(".about-us.slider .slides").slick({
-        arrows: false,
+  $(".desktop-menu ul.menu-list > li > a").click(
+    function(ev) {
+      ev.preventDefault();
+      var hrefGoTo = $(this).attr("href");
+      var position = $(hrefGoTo).offset().top - 80;
+      console.log('Menu clicked');
+    }
+  )
+  $(".overlay-menu ul li a").click(
+    function(ev) {
+      ev.preventDefault();
+      var hrefGoTo = $(this).attr("href");
+      var position = $(hrefGoTo).offset().top - 60;
+      $(".sandwich").click();
+      $(".overlay-menu").removeClass("overlay-active");
+      window.setTimeout(() => {
+        $("html, body")
+          .stop()
+          .animate({ scrollTop: position }, 500);
+      }, 0);
+    }
+  );
+  console.log('Window width is '+ windowWidth + ' px');
+  if (windowWidth < 1025) {
+    if (windowWidth < 601) {
+      $(".our-team.cards").owlCarousel({
+        stagePadding: 15,
+        autoWidth: true,
+        loop: false,
+        center: true,
+        items: 2,
+        margin: 0,
         dots: true,
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-        // autoplay: true
+        dotsEach: true
       });
     } else {
-      $(".about-us.slider .slides").slick({
-        arrows: false,
+      $(".our-team.cards").owlCarousel({
+        stagePadding: 50,
+        autoWidth: true,
+        loop: false,
+        center: false,
+        items: 3,
+        margin: 0,
         dots: true,
-        infinite: false,
-        slidesToShow: 2,
-        slidesToScroll: 1
-        // autoplay: true
+        dotsEach: true
       });
     }
-    // $(".videos.cards.slider .slides").slick({
-    //   arrows: false,
-    //   dots: true,
-    //   infinite: true,
-    //   slidesToShow: 4,
-    //   slidesToScroll: 1,
-    //   variableWidth: true
-    // });
-    $(".our-team.cards").slick({
-      arrows: false,
-      dots: true,
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      variableWidth: true
-    });
-  } else {
-    // $(".about-us.slider .slides").slick({
-    //   arrows: true,
-    //   prevArrow,
-    //   nextArrow,
-    //   dots: false,
-    //   infinite: true,
-    //   slidesToShow: 3,
-    //   slidesToScroll: 1
-    //   autoplay: true
-    // });
-    // $(".videos.cards.slider .slides").slick({
-    //   arrows: true,
-    //   prevArrow,
-    //   nextArrow,
-    //   dots: false,
-    //   infinite: true,
-    //   slidesToShow: 4,
-    //   slidesToScroll: 1
-    //   // autoplay: true
-    // });
   }
-  Array.from(menuLinks).forEach(element => {
-    element.addEventListener("mouseenter", event => {
+  Array.from(menuLinks).forEach(function(element) {
+    element.addEventListener("mouseenter", function(event) {
       event.target.style.color = colorGreen;
       event.target.style.borderBottom = "2px solid #00d9a2";
     });
-    element.addEventListener("mouseleave", event => {
+    element.addEventListener("mouseleave", function(event) {
       event.target.style.color = "inherit";
       event.target.style.borderBottom = "none";
     });
